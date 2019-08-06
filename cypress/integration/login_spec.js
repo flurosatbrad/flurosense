144e921173c2c841ce17b5875c7969598219da31
BradAir <braddong@hotmail.com>
2019-08-06 16:58:28 +1000

    draw field test complete

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,70 +1,75 @@
+import name from 'random-username-generator';
+
 describe('Login', ()=>{
 
-    const logout= ()=>cy.get('#mini-menu-log-out').click();
+    const common={
+        login: () =>{
+            cy.visit(Cypress.env('baseUrl'));
+            cy.contains('Login').click();
+            cy.get('#login-email', {timeout:60000}).type(Cypress.env('correctEmail'));
+            cy.get('#login-password').type(Cypress.env('correctPassword'))
+            cy.get('.fluro-auth__login-btn').click();
+        },
+        logout: ()=>cy.get('#mini-menu-log-out').click()
+    }
 
-    // it('should show a error message if incorrect email or password', ()=>{
-    //     cy.visit(Cypress.env('baseUrl'));
-    //     cy.contains('Login').click();
-    //     cy.get('#login-email', {timeout:60000}).type(Cypress.env('wrongName'));
-    //     cy.get('#login-password').type(Cypress.env('wrongPassword'))
-    //     cy.get('.fluro-auth__login-btn').click();
-    //     cy.contains('Wrong email or password').should('be.visible');
-    // })
-
-
-    // it('should sign up a new user via email successfully',()=>{
-    //     cy.visit(Cypress.env('baseUrl'));        
-    //     cy.contains('Sign Up').click();
-    //     cy.get('#reg-first-name').type(Cypress.env('newFirstName'));
-    //     cy.get('#reg-surname').type(Cypress.env('newSurname'));
-    //     cy.get('#reg-email').type(Cypress.env('newEmail'));
-    //     cy.get('#reg-selection-control-group-radios-toggle').click();
-    //     cy.contains('agronomist').click();
-    //     cy.get('.iti-arrow').click();
-    //     cy.contains('China').click();
-    //     cy.get('.intl-tel-input input').type(Cypress.env('newPhoneNumber'));
-    //     //this part is not working correctly 
-    //     cy.get('.fluro-auth__login-btn').click(); 
-    //     cy.contains('This email address is already registered').should('be.visible')
-    // })
+    it('should show a error message if incorrect email or password', ()=>{
+        cy.visit(Cypress.env('baseUrl'));
+        cy.contains('Login').click();
+        cy.get('#login-email', {timeout:60000}).type(Cypress.env('wrongName'));
+        cy.get('#login-password').type(Cypress.env('wrongPassword'))
+        cy.get('.fluro-auth__login-btn').click();
+        cy.contains('Wrong email or password').should('be.visible');
+    })
 
 
-    // it('should login correctly with email and password logout to sign in page',()=>{
-    //     cy.visit(Cypress.env('baseUrl'));
-    //     cy.contains('Login').click();
-    //     cy.get('#login-email', {timeout:60000}).type(Cypress.env('correctEmail'));
-    //     cy.get('#login-password').type(Cypress.env('correctPassword'))
-    //     cy.get('.fluro-auth__login-btn').click();
-    //     cy.url().should('contain', 'app/maps');
-    //     logout();
-    //     cy.url().should('contain','app/login');
+    it('should give message if trying to signup with email that has already been used',()=>{
+        cy.visit(Cypress.env('baseUrl'));        
+        cy.contains('Sign Up').click();
+        cy.get('#reg-first-name').type(Cypress.env('newFirstName'));
+        cy.get('#reg-surname').type(Cypress.env('newSurname'));
+        cy.get('#reg-email').type(Cypress.env('newEmail'));
+        cy.get('#reg-selection-control-group-radios-toggle').click();
+        cy.contains('agronomist').click();
+        cy.get('.iti-arrow').click();
+        cy.contains('China').click();
+        cy.get('.intl-tel-input input').type(Cypress.env('newPhoneNumber'));
+        cy.get('.fluro-auth__login-btn').click(); 
+        cy.contains('This email address is already registered').should('be.visible')
+    })
+
+    it('should successfully create a new user',()=>{
+        
+    })
+
+
+    it('should login correctly with email and password logout to sign in page',()=>{
+        common.login();
+        cy.url().should('contain', 'app/maps');
+        common.logout();
+        cy.url().should('contain','app/login');
     
-    // })
+    })
     
     
-    // it('should login correctly with email and password and create a farm', ()=>{
-    //     cy.visit(Cypress.env('baseUrl'));
-    //     cy.contains('Login').click();
-    //     cy.get('#login-email', {timeout:60000}).type(Cypress.env('correctEmail'));
-    //     cy.get('#login-password').type(Cypress.env('correctPassword'))
-    //     cy.get('.fluro-auth__login-btn').click();
-    //     cy.url().should('contain', 'app/maps');
-    //     cy.contains('Add Farm').click();
-    //     cy.get('input#group-name').type('New test farm');
-    //     cy.contains('Save').click();
-    //     cy.contains('Success').should('be.visible');
-    //     cy.get('.farm-feature__name', {timeout: 60000}).should('have.text', 'New test farm');
-    //     logout();
-    // })
+    it('should login correctly with email and password and create a farm', ()=>{
+        common.login();
+        cy.contains('Add Farm').click();
+        let randomName = name.generate();
+        cy.get('input#group-name').type(randomName);
+        cy.contains('Save').click();
+        cy.contains('Success').should('be.visible');
+        cy.wait(4000);
+        cy.get('.farm-feature__name', {timeout: 60000}).should('have.text', randomName);
+        cy.get('#map__select-farm').should('have.attr','title', randomName);
+        cy.get('#map__select-farm').click();
+        cy.get('#map__select-farm-menu-list').contains(randomName).should('exist')
+        common.logout();
+    })
 
     
     it('should correctly login with email and password and create a field using KML files',()=>{
-        cy.visit(Cypress.env('baseUrl'));
-        cy.contains('Login').click();
-        cy.get('#login-email', {timeout:60000}).type(Cypress.env('correctEmail'));
-        cy.get('#login-password').type(Cypress.env('correctPassword'))
-        cy.get('.fluro-auth__login-btn').click();
-        cy.url().should('contain', 'app/maps');
+        common.login();
         cy.get('#map__select-farm').click();
         cy.contains('Farm No 22').click();
         cy.contains('Add field').click();
@@ -76,8 +81,33 @@ describe('Login', ()=>{
         });    
         cy.get('#kml-name').should('have.attr', 'value','/kml/23 TOP PD')
         cy.wait(3000);
-        logout();
+        common.logout();
       
+    })
+
+    it ('should correclty login with email and password and create a field using draw function', ()=>{
+        common.login();
+        cy.get('#map__select-farm').click();
+        cy.contains('Farm No 22').click();
+        cy.contains('Add field').click();
+        cy.contains('Draw Fields').click();
+        cy.get('#fluro-map')
+        .trigger('pointerdown',{x:450,y:100})
+        .trigger('pointerup')
+        .trigger('pointerdown', {x:500, y:200})
+        .trigger('pointerup')
+        .trigger('pointerdown', {x:100, y:300})
+        .trigger('pointerup')
+        .trigger('pointerdown', {x:450, y:100})
+        let randomName = name.generate();
+        cy.get('#kml-name').clear().type(randomName)
+        cy.contains('Save fields').click();
+        cy.contains('Success').should('be.visible');
+        //wait for Success message to hide to test if name is present.
+        cy.wait(3000)
+        cy.contains(randomName).should('be.visible')
+        cy.wait(3000);
+        common.logout();
     })
         
 })
