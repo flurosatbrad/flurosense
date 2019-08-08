e4d41954d7e75fb7317c365ec952c11b10602cfb
BradAir <braddong@hotmail.com>
2019-08-08 17:25:31 +1000

    create farm
    working on location autocomplete

--- cypress/integration/farm_pages/create_farm_spec.js
+++
@@ -1,6 +0,0 @@
-describe('farm creation', ()=>{
-    it('should allow the addition of a new farm',()=>{
-        cy.login();
-        cy.visit('/app/admin/maps/136')
-    })
-})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,3 +44,21 @@ Cypress.Commands.add("login", () => {
 Cypress.Commands.add('logout', ()=>{
     cy.get('#mini-menu-log-out').click({force:true})
 })
+
+Cypress.Commands.add('randomLocation', ()=>{
+    const words= ['hello', 'farm', 'fluro', 'ball', 'water'];
+    let locationArrayLength = words.length;
+    let randomIndexNumber = Math.floor(Math.random()* locationArrayLength);
+    let myLocation = words[randomIndexNumber];
+    return myLocation;
+})
+
+Cypress.Commands.add('generate_random_string', (string_length) => { 
+  let random_string = '';
+  let random_ascii;
+  for(let i = 0; i < string_length; i++) {
+      random_ascii = Math.floor((Math.random() * 25) + 97);
+      random_string += String.fromCharCode(random_ascii)
+  }
+  return random_string
+ });
